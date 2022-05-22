746a5996a9ce45bee87a6520698c244d7a0ff330
Bartosz Witkowski <bartosz.witkowski@like-a-boss.net>
2022-05-22 17:10:42 +0200

    Made dot4 to compile

--- src/main/scala/dot.scala
+++ src/main/scala/dot.scala
@@ -148,8 +148,8 @@ object Dot3 {
 }
 
 // Dot3 with friendlier syntax
+//
 object Dot4 {
-  /*
   def dotExpr[A](
       l: Expr[Array[A]], 
       r: Expr[Array[A]])(using Quotes, Type[A], StagedNumeric[A]): Expr[A] = Log('{
@@ -162,52 +162,46 @@ object Dot4 {
       throw new RuntimeException("Arrays must have the same size (got ${lSize} ${rSize})")
     }
   })
-  */
 
   private def doDotExpr[A](
       l: Expr[Array[A]], 
       r: Expr[Array[A]])(
-      using q: Quotes,
+      using qctx: Quotes,
       typeA: Type[A],
-      sn: StagedNumeric[A]): Expr[A] = '{
-    val left = $l
-    val right = $r
+      sn: StagedNumeric[A]): Expr[A] = {
 
-    var i = 0
-    var sum: A = ${sn.zero}
+    '{
+      val left = $l
+      val right = $r
 
-    while (i < left.size) {
-      val l: A = left(i)
-      val r: A = right(i)
+      var i = 0
+      var sum: A = ${sn.zero}
 
-      val lr1: A = ${sn.times}(l, r)
-      val lr2: A = l * r
-      //sum = ${sn.plus}(sum, l * r)
+      while (i < left.size) {
+        val l: A = left(i)
+        val r: A = right(i)
 
-      i += 1
+        sum = ${ 'sum + ('l * 'r) }
+
+        i += 1
+      }
+
+      sum
     }
-
-    sum
   }
 
-  extension [A](l: A)(using sn: StagedNumeric[A]) {
-    def *(r: A)(using Quotes, Type[A]): A = '{ 
-      ${sn.times}(l, r)
-    }
-
-    /*
-    def *(r: A)(using Quotes, Type[A]): Expr[A] = '{
-      ${sn.times}(l, r)
-    }
-    */
+  extension [A](using 
+      qctx: Quotes, 
+      sn: StagedNumeric[A])(l: Expr[A]) {
+    def *(r: Expr[A]): Expr[A] = sn.times(l)(r)
+    def +(r: Expr[A]): Expr[A] = sn.plus(l)(r)
   }
 
   abstract class StagedNumeric[A] {
     def zero(using Quotes): Expr[A]
-    def plus(using Quotes): Expr[(A, A) => A]
-    def times(using Quotes): Expr[(A, A) => A]
+    def plus(using Quotes): Expr[A] => Expr[A] => Expr[A]
+    def times(using Quotes): Expr[A] => Expr[A] => Expr[A]
   }
-
 }
 
 // Dot4 for any data/accumulator
