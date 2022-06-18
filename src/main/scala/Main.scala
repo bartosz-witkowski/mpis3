c00bd7829425e7e00161fe235d65611d814d9c93
Bartosz Witkowski <bartosz.witkowski@like-a-boss.net>
2022-06-18 17:27:05 +0200

    Dot product using the vector api

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -116,6 +116,73 @@ object Main {
 
       println(r)
     }
+
+    {
+      import scala.quoted.*
+      import Dot6._
+
+      import jdk.incubator.vector.{IntVector, VectorOperators}
+
+      given StagedVectorized[Int] = new StagedVectorized[Int] {
+        override def zero(using Quotes): Expr[Int] = Expr(0)
+        override def plus(using Quotes): Expr[Int] => Expr[Int] => Expr[Int] = {
+          l => r => '{ $l + $r }
+        }
+        override def times(using Quotes): Expr[Int] => Expr[Int] => Expr[Int] = {
+          l => r => '{ $l * $r }
+        }
+
+        override type Vector = IntVector
+        override def vectorType(using Quotes): Type[Vector] = Type.of[IntVector]
+
+        override def vectorFromArray(using Quotes): Expr[Array[Int]] => Expr[Int] => Expr[Vector] = {
+          array => offset => '{ IntVector.fromArray(IntVector.SPECIES_PREFERRED, $array, $offset) }
+        }
+        override def vectorLengthValue: Int = IntVector.SPECIES_PREFERRED.length
+        override def vectorLength(using Quotes): Expr[Int] = '{ IntVector.SPECIES_PREFERRED.length }
+        override def loopBound(using Quotes): Expr[Int] => Expr[Int] = {
+          arraySize => '{ IntVector.SPECIES_PREFERRED.loopBound($arraySize) }
+        }
+
+        override def mul(using Quotes): Expr[Vector] => Expr[Vector] => Expr[Vector] = {
+          l => r => '{ $l.mul($r) }
+        }
+
+        override def add(using Quotes): Expr[Vector] => Expr[Vector] => Expr[Vector] = {
+          l => r => '{ $l.add($r) }
+        }
+
+        override def sum(using Quotes): Expr[Vector] => Expr[Int] = {
+          v => '{ $v.reduceLanes(VectorOperators.ADD) }
+        }
+      }
+
+      given StagedNumeric[Long] = new StagedNumeric[Long] {
+        override def zero(using Quotes): Expr[Long] = Expr(0l)
+        override def plus(using Quotes): Expr[Long] => Expr[Long] => Expr[Long] = {
+          l => r => '{ $l + $r }
+        }
+        override def times(using Quotes): Expr[Long] => Expr[Long] => Expr[Long] = {
+          l => r => '{ $l * $r }
+        }
+      }
+
+      given StagedPromote[Int, Long] = new StagedPromote[Int, Long] {
+        override def apply(using Quotes): Expr[Int] => Expr[Long] = { int =>
+          '{ $int.toLong }
+        }
+      }
+      
+      val r = staging.run[Long] { 
+        Dot6.dotExpr[Int, Long](
+          //Expr(Array(1)),
+          Expr(Array(1, 3, -5,  1, 3, -5,  1, 3)), 
+          //Expr(Array(1)))
+          Expr(Array(4, -2, -1, 4, -2, -1, 4, -2)))
+      }
+
+      println(r)
+    }
   }
 
   def power(): Unit = {
--- src/main/scala/dot.scala
+++ src/main/scala/dot.scala
@@ -269,6 +269,152 @@ object Dot5 {
   }
 }
 
+import scala.reflect.ClassTag
+
+private[mpis3] given arrayFromExpr[T](using Type[T], FromExpr[T], ToExpr[Array[T]], ClassTag[T]): FromExpr[Array[T]] = new FromExpr[Array[T]] {
+  def unapply(x: Expr[Array[T]])(using quotes: Quotes) = {
+    import quotes.reflect.*
+
+    def get(x: Term): Option[Array[T]] = x match {
+      case Inlined(
+          Some(TypeIdent("ToExpr$")), 
+          Nil, 
+          Apply(Select(Ident("Array"), "apply"), list)) => 
+        list match {
+          case Nil =>
+            Some(Array.empty[T])
+          case x :: xs =>
+            x.asExprOf[T].value.flatMap { head =>
+              xs match {
+                case List(Typed(Inlined(_, _, xs), _)) => 
+                  xs.asExprOf[Seq[T]].value.map { tail =>
+                    (head +: tail).toArray
+                  }
+                case other =>
+                  None
+              }
+           }
+        }
+    
+      case other => 
+        None
+    }
+
+    get(x.asTerm)
+  }
+}
+
 /*
- * Dot product using the vector api?
+ * Dot5 using the vector api
  */
+object Dot6 {
+  def dotExpr[A, B](
+        l: Expr[Array[A]], 
+        r: Expr[Array[A]])(using Quotes, Type[A], FromExpr[A], ToExpr[Array[A]], ToExpr[A], ClassTag[A], Type[B], StagedVectorized[A], StagedNumeric[B], StagedPromote[A, B]): Expr[B] = {
+      // does this even make sense? we would ideally want to call it for a known l and many unknown r-s?
+      val lArray: Array[A] = l.valueOrAbort
+      val rArray: Array[A] = r.valueOrAbort
+      
+      if (lArray.size != rArray.size) {
+        throw new RuntimeException("array sizes must match")
+      } else {
+        Log(doDotExpr(lArray, rArray))
+      }
+    }
+
+  private def doDotExpr[A, B](
+      _left: Array[A], 
+      _right: Array[A])(
+      using qctx: Quotes,
+      typeA: Type[A],
+      typeB: Type[B],
+      toExprArrA: ToExpr[Array[A]],
+      toExprA: ToExpr[A],
+      na: StagedVectorized[A],
+      nb: StagedNumeric[B],
+      promote: StagedPromote[A, B]): Expr[B] = {
+    given vectorType: Type[na.Vector] = na.vectorType
+
+    val vectorLength = na.vectorLengthValue
+
+    if (_left.size % vectorLength == 0) '{
+      val left: Array[A] = ${Expr(_left)}
+      val right: Array[A] = ${Expr(_right)}
+      var i = 0
+      var sum: B = ${nb.zero}
+
+      val bound: Int = ${na.loopBound(Expr(_left.length))}
+
+      while (i < bound) {
+        val l: na.Vector = ${na.vectorFromArray('left)('i)}
+        val r: na.Vector = ${na.vectorFromArray('right)('i)}
+        
+        val vmul = ${na.mul('l)('r)}
+        sum = ${ 'sum + promote(na.sum('vmul)) }
+
+        i += ${ na.vectorLength }
+      }
+
+      sum
+    } else '{
+      val left: Array[A] = ${Expr(_left)}
+      val right: Array[A] = ${Expr(_right)}
+
+      var i = 0
+      var sum: B = ${nb.zero}
+
+      val bound: Int = ${na.loopBound('{left.length})}
+
+      while (i < bound) {
+        val l: na.Vector = ${na.vectorFromArray('left)('i)}
+        val r: na.Vector = ${na.vectorFromArray('right)('i)}
+        
+        val vmul = ${na.mul('l)('r)}
+        sum = ${ 'sum + promote(na.sum('vmul)) }
+
+        i += ${ na.vectorLength }
+      }
+
+      while (i < ${Expr(_left.size)}) {
+        val l: A = left(i)
+        val r: A = right(i)
+
+        sum = ${ 'sum + promote('l * 'r) }
+
+        i += 1
+      }
+
+      sum
+    }
+  }
+
+  extension [A](l: Expr[A]) {
+    def *(using q: Quotes, sn: StagedNumeric[A])(r: Expr[A]): Expr[A] = sn.times(l)(r)
+    def +(using q: Quotes, sn: StagedNumeric[A])(r: Expr[A]): Expr[A] = sn.plus(l)(r)
+  }
+
+  abstract class StagedNumeric[A] {
+    def zero(using Quotes): Expr[A]
+    def plus(using Quotes): Expr[A] => Expr[A] => Expr[A]
+    def times(using Quotes): Expr[A] => Expr[A] => Expr[A]
+  }
+
+  abstract class StagedVectorized[A] extends StagedNumeric[A] {
+    type Vector
+    def vectorType(using Quotes): Type[Vector]
+
+    def vectorLengthValue: Int
+    def vectorLength(using Quotes): Expr[Int]
+    def vectorFromArray(using Quotes): Expr[Array[A]] => Expr[Int] => Expr[Vector]
+    def loopBound(using Quotes): Expr[Int] => Expr[Int]
+    def mul(using Quotes): Expr[Vector] => Expr[Vector] => Expr[Vector]
+    def add(using Quotes): Expr[Vector] => Expr[Vector] => Expr[Vector]
+    def sum(using Quotes): Expr[Vector] => Expr[A]
+  }
+
+  abstract class StagedPromote[A, B] {
+    def apply(using Quotes): Expr[A] => Expr[B]
+  }
+}
+
+// Dot7 - Dot6 without code duplication
